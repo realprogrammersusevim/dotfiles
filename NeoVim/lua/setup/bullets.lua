fd956ec4af4ca430fa97d1f5ab79cb087d18d4a4
Jonathan Milligan <logouts.deco-0e@icloud.com>
2022-10-26 15:23:12 -0500

    Add bullets.vim and filetype config
    Added the plugin bullets.vim which I was missing badly. I also finally
    figured out filetype specific configurations, so I added an option to
    automatically turn on spell checking in markdown files.

---
+++ NeoVim/lua/setup/bullets.lua
@@ -0,0 +1,4 @@
+vim.g.bullets_enabled_file_types = { "markdown", "text", "gitcommit", "scratch" }
+vim.g.bullets_delete_last_bullet_if_empty = 1
+vim.g.bullets_pad_right = 0
+-- Eventually disable the default mappings and move them all to Which-Key.
