23092d86c4d572329ec726476d7dc4ce08a11f6a
Jonathan <git@jonathansm.dev>
2023-04-04 12:06:45 -0500

    fix(nvim): Update deprecated barbar options
    The Neovim plugin I have for managing tabs updated some of it's options
    and deprecated the config options I was using. All the deprecated code
    is now fixed.
    
    Updated all the plugins as well.

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -10,8 +10,6 @@ return {
       auto_hide = true,
       -- Enable/disable current/total tabpages indicator (top right corner)
       tabpages = true,
-      -- Enable/disable close button
-      closable = false,
       -- Enables/disable clickable tabs
       --  - left-click: go to buffer
       --  - middle-click: delete buffer
@@ -24,18 +22,20 @@ return {
       -- Enable/disable icons
       -- if set to 'numbers', will show buffer index in the tabline
       -- if set to 'both', will show buffer index and icons in the tabline
-      icons = true,
+      icons = {
+        filetype = { enabled = true },
+        modified = { modified = { button = '●' } },
+        pinned = { button = '車' },
+        separator = { left = '▎' },
+        inactive = { separator = { left = '▎' } },
+        button = '',
+      },
       -- If set, the icon color will follow its corresponding buffer
       -- highlight group. By default, the Buffer*Icon group is linked to the
       -- Buffer* group (see Highlighting below). Otherwise, it will take its
       -- default value as defined by devicons.
       icon_custom_colors = false,
       -- Configure icons on the bufferline.
-      icon_separator_active = '▎',
-      icon_separator_inactive = '▎',
-      icon_close_tab = '',
-      icon_close_tab_modified = '●',
-      icon_pinned = '車',
       -- If true, new buffers will be inserted at the start/end of the list.
       -- Default is to insert after current buffer.
       insert_at_end = false,
