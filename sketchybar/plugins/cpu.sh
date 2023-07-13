d4e5468dd95dec945bfcd3934cb9a83629547c93
Jonathan <git@jonathansm.dev>
2023-07-12 22:30:40 -0500

    fix(sketchybar): round cpu usage
    Rounded the CPU usage percentage that was displayed into a nice integer.
    The number is now more predictable.

--- sketchybar/plugins/cpu.sh
+++ sketchybar/plugins/cpu.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-PERCENTAGE=$(ps -A -o %cpu | awk '{s+=$1} END {print s}')
+PERCENTAGE=$(ps -A -o %cpu | awk '{s+=$1} END {print s}' | xargs printf '%0.f')
 
 sketchybar --set "$NAME" label="${PERCENTAGE}%"
