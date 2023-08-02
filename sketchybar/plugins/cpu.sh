953d6f9c2c23638e6713e71c12de36fb49aa2a00
Jonathan <git@jonathansm.dev>
2023-08-02 12:44:18 -0500

    fix(sketchybar): make cpu usage more reliable
    Now using top's numbers to get the CPU usage. Also means percentage
    total will add up to 100%.

--- sketchybar/plugins/cpu.sh
+++ sketchybar/plugins/cpu.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-PERCENTAGE=$(ps -A -o %cpu | awk '{s+=$1} END {print s}' | xargs printf '%0.f')
+# Extract the numbers from the CPU usage line
+NUMS=$(top -l 1 -n 0 | grep "CPU usage:" | grep -o "[0-9]\+.[0-9]\+")
+ARR_NUMS=($NUMS) # Convert the string to an array so it can be indexed
+# Floating point math has to be done with bc. Use printf as a poor mans math.floor
+PERCENTAGE=$(bc <<< "scale=2; ${ARR_NUMS[0]}+${ARR_NUMS[1]}" | xargs printf '%0.f')
 
 sketchybar --set "$NAME" label="${PERCENTAGE}%"
