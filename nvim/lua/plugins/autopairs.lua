e74f98b322745304059a65ad0ccf1aa4a38bd870
Jonathan <git@jonathansm.dev>
2023-08-11 12:37:11 -0500

    feat(nvim): add more autopairs for inline markdown
    Added autopairs for `*`, `~`, and `_` for Markdown filetypes. Everything
    should be nicely paired except when in code spans or fenced blocks.

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -2,6 +2,36 @@ return {
   {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
-    config = true,
+    config = function()
+      local npairs = require('nvim-autopairs')
+
+      npairs.setup({
+        check_ts = true,
+      })
+
+      local rule = require('nvim-autopairs.rule')
+      local ts_conds = require('nvim-autopairs.ts-conds')
+
+      npairs.add_rules({
+        rule('*', '*', 'markdown'):with_pair(ts_conds.is_not_ts_node({
+          'code_fence_content',
+          'code_span',
+          'strong_emphasis',
+          'strong_emphasis_delimiter',
+        })),
+        rule('_', '_', 'markdown'):with_pair(ts_conds.is_not_ts_node({
+          'code_fence_content',
+          'code_span',
+          'emphasis',
+          'emphasis_delimiter',
+        })),
+        rule('~', '~', 'markdown'):with_pair(ts_conds.is_not_ts_node({
+          'code_fence_content',
+          'code_span',
+          'strikethrough',
+          'strikethrough_delimiter',
+        })),
+      })
+    end,
   },
 }
