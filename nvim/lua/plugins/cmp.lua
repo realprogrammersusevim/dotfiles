50e799bdf60af0c02b00695b9de6977f715adcad
Jonathan <git@jonathansm.dev>
2023-09-28 12:17:40 -0500

    feat(nvim): latex fraction luasnip
    Created a new Markdown snippet for LaTeX fractions.

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -9,7 +9,7 @@ return {
         'L3MON4D3/LuaSnip',
         config = function()
           require('luasnip.loaders.from_vscode').lazy_load()
-          require('luasnip.loaders.from_lua').load({ paths = './snippets' })
+          require('luasnip.loaders.from_lua').lazy_load({ paths = './snippets' })
         end,
         dependencies = {
           'saadparwaiz1/cmp_luasnip',
--- nvim/snippets/markdown.lua
+++ nvim/snippets/markdown.lua
@@ -3,4 +3,14 @@ return {
     { trig = 'multmath', dscr = 'Multiline aligned LaTeX equation' },
     { t({ '$$', '\\begin{aligned}', '' }), i(1, 'math'), t({ '', '\\end{aligned}', '$$' }) }
   ),
+  s({
+    trig = 'fraction',
+    desc = 'LaTeX fraction',
+  }, {
+    t('\\frac{'),
+    i(1),
+    t('}{'),
+    i(2),
+    t('}'),
+  }),
 }
