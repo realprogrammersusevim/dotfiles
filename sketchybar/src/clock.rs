f8b724637eedf75496f055213cef379488d5cfe9
Jonathan <git@jonathansm.dev>
2023-10-06 13:08:26 -0500

    feat(sketchybar): add cpu plugin in rust
    Rewrote the cpu plugin in Rust for better performance and lower battery
    usage.

--- sketchybar/src/clock.rs
+++ sketchybar/src/clock.rs
@@ -7,7 +7,7 @@ fn main() {
         .expect("Something went horribly wrong and Sketchybar didn't create the $NAME env var"); // Just panic and end the program
     let dt = Local::now();
     let formatted = dt.format("%a, %h %e - %l:%M").to_string();
-    println!("{formatted}");
+
     Command::new("sketchybar")
         .arg("--set")
         .arg(name)
