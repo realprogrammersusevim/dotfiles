b228a7949a22ce35c58b6323e0faf4eb143aa7b6
Jonathan <git@jonathansm.dev>
2024-03-15 12:48:23 +0800

    fix(sketchybar): move .env source to inside plugin
    Moved the sourcing of the .env to inside the bitcoin plugin rather than
    trying to do it in the sketchybarrc.

--- sketchybar/plugins/bitcoin.sh
+++ sketchybar/plugins/bitcoin.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-PERCENTAGE="$(curl -s --data-binary '{"jsonrpc": "1.0", "id": "curltest", "method": "getblockchaininfo", "params": []}' -H 'content-type: text/plain;' http://$BITCOIN_USER:$BITCOIN_PASSWORD@127.0.0.1:8332 | jq .result.verificationprogress | xargs -I {} echo "scale=2; {}*100" | bc | xargs printf "%.3f\n")"
+source .env
+PERCENTAGE="$(curl -s --data-binary '{"jsonrpc": "1.0", "id": "curltest", "method": "getblockchaininfo", "params": []}' -H 'content-type: text/plain;' "http://$BITCOIN_USER:$BITCOIN_PASSWORD@127.0.0.1:8332" | jq .result.verificationprogress | xargs -I {} echo "scale=2; {}*100" | bc | xargs printf "%.3f\n")"
 
 sketchybar --set "$NAME" label="${PERCENTAGE}%"
