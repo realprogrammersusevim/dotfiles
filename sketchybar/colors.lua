59786566c39fe995ca6a97a6a117fc22bcc02b71
Jonathan <git@jonathansm.dev>
2024-04-14 13:26:16 -0500

    fix(sketchybar): removed incompatible unused color
    Removed a color that was incompatible with LuaJit and wasn't used
    anyway.

--- sketchybar/colors.lua
+++ sketchybar/colors.lua
@@ -43,11 +43,4 @@ return {
   },
   bg1 = 0xff363944,
   bg2 = 0xff414550,
-
-  with_alpha = function(color, alpha)
-    if alpha > 1.0 or alpha < 0.0 then
-      return color
-    end
-    return (color & 0x00ffffff) | (math.floor(alpha * 255.0) << 24)
-  end,
 }
