35b10a2ad781f0b3d6fae48db7882c8cc6db6721
Jonathan <git@jonathansm.dev>
2024-04-16 17:12:15 -0500

    feat(sketchybar): add vpn item
    Added the VPN item to show the current connection status and connect and
    disconnect with a click.

---
+++ sketchybar/items/vpn.lua
@@ -0,0 +1,19 @@
+local vpn = Sbar.add('alias', 'Mullvad VPN,Item-0', {
+  position = 'right',
+  background = {
+    drawing = false,
+  },
+  update_freq = 10,
+})
+
+vpn:subscribe('mouse.clicked', function()
+  Sbar.exec('mullvad status', function(output)
+    print(output)
+    if output:match('^Disconnected.+') then
+      print('Connecting to Mullvad VPN...')
+      Sbar.exec('mullvad connect')
+    else
+      Sbar.exec('mullvad disconnect')
+    end
+  end)
+end)
