2f52b09ebfecc7b0b9f9d1abee51593c8868a6d9
Jonathan <git@jonathansm.dev>
2024-06-27 13:17:37 -0500

    fix(nvim): disable copilot
    Disabled Copilot for a bit until I get access again.

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,6 +1,7 @@
 return {
   {
     'zbirenbaum/copilot.lua', -- Lua replacement for Copilot
+    enabled = false,
     event = 'InsertEnter',
     opts = {
       suggestion = { auto_trigger = true, keymap = { accept = '<C-CR>' } },
