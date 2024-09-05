654b5a8bc93dc86b08698cace7dc6d526f6d7b72
Jonathan <git@jonathansm.dev>
2024-09-05 13:52:05 -0500

    style(zsh): format personal_commands.sh
    Formatted the personal commands file.

--- zsh/personal_commands.sh
+++ zsh/personal_commands.sh
@@ -5,7 +5,7 @@
 # If you want another command simply create a new function
 
 zen() {
-    python3 -c "import this"
+  python3 -c "import this"
 }
 
 ##
@@ -13,77 +13,77 @@ zen() {
 # Usage: `fif` or `fif <folder>`.
 #
 fif() {
-    [[ -n $1 ]] && cd "$1" # go to provided folder or noop
-    RG_DEFAULT_COMMAND="rg -i -l --hidden --no-ignore-vcs"
+  [[ -n $1 ]] && cd "$1" # go to provided folder or noop
+  RG_DEFAULT_COMMAND="rg -i -l --hidden"
 
-    selected=$(
-        FZF_DEFAULT_COMMAND="rg --files" fzf \
-            -m \
-            -e \
-            --ansi \
-            --disabled \
-            --reverse \
-            --bind "ctrl-a:select-all" \
-            --bind "f12:execute-silent:(subl -b {})" \
-            --bind "change:reload:$RG_DEFAULT_COMMAND {q} || true" \
-            --preview "rg -i --context 10 {q} {} | bat --file-name {} -f --plain -H 11 2>/dev/null"
-    )
+  selected=$(
+    FZF_DEFAULT_COMMAND="rg --files" fzf \
+      -m \
+      -e \
+      --ansi \
+      --disabled \
+      --reverse \
+      --bind "ctrl-a:select-all" \
+      --bind "f12:execute-silent:(subl -b {})" \
+      --bind "change:reload:$RG_DEFAULT_COMMAND {q} || true" \
+      --preview "rg -i --context 10 {q} {} | bat --file-name {} -f --plain -H 11 2>/dev/null"
+  )
 
-    [[ -n $selected ]] && nvim "$selected" # open multiple files in editor
+  [[ -n $selected ]] && nvim "$selected" # open multiple files in editor
 }
 
 ##
 # Interactive Find File search.
 # Usage: `ff` or `fif <folder>`.
 ff() {
-    [[ -n $1 ]] && cd "$1" # go to provided folder or noop
-    RG_DEFAULT_COMMAND="rg -i"
-    FILES="fd -t f -H"
+  [[ -n $1 ]] && cd "$1" # go to provided folder or noop
+  RG_DEFAULT_COMMAND="rg -i"
+  FILES="fd -t f -H"
 
-    selected=$(
-        FZF_DEFAULT_COMMAND="fd -t f -H" fzf \
-            -m \
-            -e \
-            --ansi \
-            --disabled \
-            --reverse \
-            --bind "ctrl-a:select-all" \
-            --bind "f12:execute-silent:(subl -b {})" \
-            --bind "change:reload:$FILES | $RG_DEFAULT_COMMAND {q} || true" \
-            --preview "bat {} -f --plain 2>/dev/null"
-    )
+  selected=$(
+    FZF_DEFAULT_COMMAND="fd -t f -H" fzf \
+      -m \
+      -e \
+      --ansi \
+      --disabled \
+      --reverse \
+      --bind "ctrl-a:select-all" \
+      --bind "f12:execute-silent:(subl -b {})" \
+      --bind "change:reload:$FILES | $RG_DEFAULT_COMMAND {q} || true" \
+      --preview "bat {} -f --plain 2>/dev/null"
+  )
 
-    [[ -n $selected ]] && nvim "$selected" # open multiple files in editor
+  [[ -n $selected ]] && nvim "$selected" # open multiple files in editor
 }
 
 nvims() {
-    items=("personal" "plain" "lazy")
-    config=$(printf "%s\n" "${items[@]}" | /opt/homebrew/bin/fzf --prompt="  Neovim config > " --height=~50% --layout=reverse --border --exit-0)
+  items=("personal" "plain" "lazy")
+  config=$(printf "%s\n" "${items[@]}" | /opt/homebrew/bin/fzf --prompt="  Neovim config > " --height=~50% --layout=reverse --border --exit-0)
 
-    if [[ -z $config ]]; then
-        echo "Nothing selected"
-        return 0
-    elif  [[ $config == "personal" ]]; then
-        config=""
-    fi
+  if [[ -z $config ]]; then
+    echo "Nothing selected"
+    return 0
+  elif [[ $config == "personal" ]]; then
+    config=""
+  fi
 
-    NVIM_APPNAME=$config nvim "$@"
+  NVIM_APPNAME=$config nvim "$@"
 }
 
 # Flash the screen until a key is pressed
 flasher() {
-    while true; do
-        printf "\\e[?5h";
-        sleep 0.1;
-        printf "\\e[?5l";
-        read -r -s -t 1 -k && break;
-    done;
+  while true; do
+    printf "\\e[?5h"
+    sleep 0.1
+    printf "\\e[?5l"
+    read -r -s -t 1 -k && break
+  done
 }
 
 # Change the kitty theme
 theme() {
-    items=("night" "day" "moon" "storm")
-    theme=$(printf "%s\n" "${items[@]}" | /opt/homebrew/bin/fzf --prompt=" Theme > " --height=~50% --layout=reverse --border --exit-0) # Use fzf as a theme picker
-    cp ~/.config/kitty/themes/tokyonight_"$theme".conf ~/.config/kitty/current.conf # The current.conf is included in the main kitty.conf
-    kill -SIGUSR1 $(pgrep -a kitty) # Sending the SIGUSR1 signal to the kitty process reloads the config
+  items=("night" "day" "moon" "storm")
+  theme=$(printf "%s\n" "${items[@]}" | /opt/homebrew/bin/fzf --prompt=" Theme > " --height=~50% --layout=reverse --border --exit-0) # Use fzf as a theme picker
+  cp ~/.config/kitty/themes/tokyonight_"$theme".conf ~/.config/kitty/current.conf                                                    # The current.conf is included in the main kitty.conf
+  kill -SIGUSR1 $(pgrep -a kitty)                                                                                                    # Sending the SIGUSR1 signal to the kitty process reloads the config
 }
