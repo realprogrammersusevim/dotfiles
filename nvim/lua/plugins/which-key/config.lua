ab6f949115f8c013ed2338c0443926cc309db6ba
Jonathan <git@jonathansm.dev>
2024-09-22 14:24:04 -0500

    refactor(nvim): update which-key config to v2
    Which-key has a new way of specifying keymappings. Updated my config to
    use that.

--- nvim/lua/plugins/which-key/config.lua
+++ nvim/lua/plugins/which-key/config.lua
@@ -20,34 +20,34 @@ require('which-key').setup({
   },
   -- add operators that will trigger motion and text object completion
   -- to enable all native operators, set the preset / operators plugin above
-  operators = { gc = 'Comments' },
+  -- operators = { gc = 'Comments' },
   icons = {
     breadcrumb = '»', -- symbol used in the command line area that shows your active key combo
     separator = '➜', -- symbol used between a key and it's label
     group = '+', -- symbol prepended to a group
   },
-  popup_mappings = {
-    scroll_down = '<c-d>', -- binding to scroll down inside the popup
-    scroll_up = '<c-u>', -- binding to scroll up inside the popup
-  },
-  window = {
-    border = 'none', -- none, single, double, shadow
-    position = 'bottom', -- bottom, top
-    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-    padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-    winblend = 0,
-  },
+  -- popup_mappings = {
+  --   scroll_down = '<c-d>', -- binding to scroll down inside the popup
+  --   scroll_up = '<c-u>',   -- binding to scroll up inside the popup
+  -- },
+  -- window = {
+  --   border = 'none',          -- none, single, double, shadow
+  --   position = 'bottom',      -- bottom, top
+  --   margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
+  --   padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+  --   winblend = 0,
+  -- },
   layout = {
     height = { min = 4, max = 25 }, -- min and max height of the columns
     width = { min = 20, max = 50 }, -- min and max width of the columns
     spacing = 3, -- spacing between columns
     align = 'left', -- align columns left, center or right
   },
-  ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
-  hidden = { '<silent>', '<CMD>', '<Cmd>', '<CR>', 'call', 'lua', '^:', '^ ' }, -- hide mapping boilerplate
+  -- ignore_missing = false,           -- enable this to hide mappings for which you didn't specify a label
+  -- hidden = { '<silent>', '<CMD>', '<Cmd>', '<CR>', 'call', 'lua', '^:', '^ ' }, -- hide mapping boilerplate
   show_help = true, -- show help message on the command line when the popup is visible
   show_keys = true, -- show the currently pressed key and its label as a message in the command line
-  triggers = 'auto', -- automatically setup triggers
+  -- triggers = 'auto', -- automatically setup triggers
   -- triggers = {"<leader>"} -- or specify a list manually
   -- disable the WhichKey popup for certain buf types and file types.
   -- Disabled by deafult for Telescope
--- nvim/lua/plugins/which-key/keymappings.lua
+++ nvim/lua/plugins/which-key/keymappings.lua
@@ -1,110 +1,283 @@
-require('which-key').register({
-  w = { name = 'Save', { '<CMD>w!<CR>', 'Save' } },
-  W = { name = 'Save without formatting', { '<CMD>noautocmd w<CR>', 'Save without formatting' } }, -- When you don't want the formatter to fire while saving
-  Q = { name = 'Quit', { '<CMD>qa<CR>', '[Q]uit' } }, -- Uppercase makes it harder to bump next to w
-  h = { name = 'No [h]ighlight', { '<CMD>nohl<CR>', 'No [h]ighlight' } },
-  P = {
-    name = 'Lazy [p]lugins',
-    h = { '<CMD>Lazy home<CR>', 'Lazy [h]ome' },
-    s = {
-      function()
-        print(require('utils').plugin_time())
-      end,
-      'Lazy [s]tats',
-    },
+require('which-key').add({
+  {
+    '<leader>w',
+    '<CMD>w!<CR>',
+    desc = 'Save',
   },
-  s = {
-    name = 'Search',
-    s = { '<CMD>Telescope<CR>', 'Search' },
-    b = { '<CMD>Telescope git_branches<CR>', '[b]ranches' },
-    c = { '<CMD>Telescope colorscheme<CR>', '[c]olorscheme' },
-    f = { '<CMD>Telescope find_files<CR>', '[f]ile' },
-    h = { '<CMD>Telescope help_tags<CR>', '[h]elp' },
-    H = { '<CMD>Telescope highlights<CR>', '[H]ighlight groups' },
-    M = { '<CMD>Telescope man_pages<CR>', '[M]an pages' },
-    r = { '<CMD>Telescope oldfiles<CR>', '[r]ecent file' },
-    R = { '<CMD>Telescope registers<CR>', '[R]egisters' },
-    t = { '<CMD>Telescope live_grep<CR>', '[t]ext' },
-    T = { '<CMD>TodoTelescope<CR>', '[T]odos' },
-    k = { '<CMD>Telescope keymaps<CR>', '[k]eymaps' },
-    C = { '<CMD>Telescope commands<CR>', '[C]ommands' },
-    p = { '<CMD>Telescope planets<CR>', '[p]lanets' },
+  {
+    '<leader>W',
+    '<CMD>noautocmd w<CR>',
+    desc = 'Save without formatting',
   },
-  t = {
-    name = '[t]rouble',
-    t = { '<CMD>TroubleToggle<CR>', '[t]rouble' },
-    n = {
-      '<CMD>lua require("trouble").next({ skip_groups = true, jump = true })<CR>',
-      '[n]ext',
-    },
-    p = {
-      '<CMD>lua require("trouble").previous({ skip_groups = true, jump = true })<CR>',
-      '[p]revious',
-    },
+  {
+    '<leader>Q',
+    '<CMD>qa<CR>',
+    desc = '[Q]uit',
   },
-  g = {
-    name = '[g]it',
-    c = { '<CMD>Telescope git_commits<CR>', 'Checkout [c]ommit' },
-    C = {
-      '<CMD>Telescope git_bcommits<CR>',
-      '[C]heckout commit(for current file)',
-    },
-    s = { '<CMD>Gitsigns stage_hunk<CR>', '[s]tage hunk' },
-    u = { '<CMD>Gitsigns undo_stage_hunk<CR>', '[u]ndo stage hunk' },
-    r = { '<CMD>Gitsigns reset_hunk<CR>', '[r]eset hunk' },
-    p = { '<CMD>Gitsigns preview_hunk<CR>', '[p]review hunk' },
-    n = { '<CMD>Gitsigns next_hunk<CR>', '[n]ext hunk' },
-    b = { '<CMD>Gitsigns prev_hunk<CR>', '[back] hunk' },
+  {
+    '<leader>h',
+    '<CMD>nohl<CR>',
+    desc = 'No [h]ighlight',
   },
-  l = {
-    name = 'LSP',
-    r = { '<CMD>lua vim.lsp.buf.rename()<CR>', '[r]ename' },
-    i = { '<CMD>lua vim.lsp.buf.hover()<CR>', '[i]nformation' },
-    d = { '<CMD>lua vim.lsp.buf.definition()<CR>', '[d]efinition' },
-    D = { '<CMD>lua vim.lsp.buf.declaration()<CR>', '[D]eclaration' },
-    m = { '<CMD>lua vim.lsp.buf.implementation()<CR>', 'I[m]plementation' },
-    q = { '<CMD>lua vim.lsp.buf.references()<CR>', '[q]uickfix' },
-    e = { '<CMD>lua vim.diagnostic.open_float()<CR>', '[e]rrors' },
-    a = { '<CMD>lua vim.lsp.buf.code_action()<CR>', 'Code [a]ction' },
+  { '<leader>P', group = 'Lazy [Plugins]' },
+  {
+    '<leader>Ph',
+    '<CMD>Lazy home<CR>',
+    desc = 'Lazy [h]ome',
   },
-  L = {
-    name = 'Treesitter [L]SP',
-    i = {
-      function()
-        local type = require('nvim-treesitter.ts_utils').get_node_at_cursor():type()
-        type = type:gsub('^%l', string.upper) -- Uppercase the first char
-        type = type:gsub('_', ' ') -- Replace underscores with spaces
-        vim.lsp.util.open_floating_preview({ type }) -- Show our nicely formatted TS node type in an LSP floating window
-      end,
-      '[i]nformation',
-    },
+  {
+    '<leader>Ps',
+    function()
+      print(require('utils').plugin_time())
+    end,
+    desc = 'Lazy [s]tats',
   },
-  m = {
-    name = 'Markdown',
-    z = { '<CMD>ZoteroCite<CR>', '[z]otero cite' },
-    l = {
-      '<CMD>MarkdownFollowLink<CR>',
-      'Follow Markdown Link',
-    },
-    t = { '<CMD>ThesaurusReplace<CR>', '[t]hesaurus replace' },
+  { '<leader>s', group = 'Search' },
+  {
+    '<leader>ss',
+    '<CMD>Telescope<CR>',
+    desc = 'Search',
   },
-  r = {
-    '[r]estore session',
-    s = { 'lua require("persistence").load()', '[s]ession for current directory' },
-    l = { '<CMD>lua require("persistence").load({ last = true })<CR>', '[l]ast session' },
+  {
+    '<leader>sb',
+    '<CMD>Telescope git_branches<CR>',
+    desc = '[b]ranches',
   },
-  d = {
-    name = '[d]ebug',
-    c = { '<CMD>lua require("dap").continue()<CR>', '[c]ontinue' },
-    i = { '<CMD>lua require("dap").step_into()<CR>', 'Step [i]nto' },
-    o = { '<CMD>lua require("dap").step_over()<CR>', 'Step [o]ver' },
-    O = { '<CMD>lua require("dap").step_out()<CR>', 'Step [o]ut' },
-    b = { '<CMD>lua require("dap").toggle_breakpoint()<CR>', 'Toggle [b]reakpoint' },
-    B = {
-      '<CMD>lua require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>',
-      'Set breakpoint',
-    },
-    t = { '<CMD>lua require("dapui").toggle()<CR>', 'DapUI [t]oggle' },
+  {
+    '<leader>sc',
+    '<CMD>Telescope colorscheme<CR>',
+    desc = '[c]olorscheme',
   },
-  y = { name = '[y]ank', { '<CMD>call setreg("+", @")<CR>', 'Copy to clipboard' } },
-}, { prefix = '<leader>' })
+  {
+    '<leader>sf',
+    '<CMD>Telescope find_files<CR>',
+    desc = '[f]ile',
+  },
+  {
+    '<leader>sh',
+    '<CMD>Telescope help_tags<CR>',
+    desc = '[h]elp',
+  },
+  {
+    '<leader>sH',
+    '<CMD>Telescope highlights<CR>',
+    desc = '[H]ighlight groups',
+  },
+  {
+    '<leader>sM',
+    '<CMD>Telescope man_pages<CR>',
+    desc = '[M]an pages',
+  },
+  {
+    '<leader>sr',
+    '<CMD>Telescope oldfiles<CR>',
+    desc = '[r]ecent file',
+  },
+  {
+    '<leader>sR',
+    '<CMD>Telescope registers<CR>',
+    desc = '[R]egisters',
+  },
+  {
+    '<leader>st',
+    '<CMD>Telescope live_grep<CR>',
+    desc = '[t]ext',
+  },
+  {
+    '<leader>sT',
+    '<CMD>TodoTelescope<CR>',
+    desc = '[T]odos',
+  },
+  {
+    '<leader>sk',
+    '<CMD>Telescope keymaps<CR>',
+    desc = '[k]eymaps',
+  },
+  {
+    '<leader>sC',
+    '<CMD>Telescope commands<CR>',
+    desc = '[C]ommands',
+  },
+  {
+    '<leader>sp',
+    '<CMD>Telescope planets<CR>',
+    desc = '[p]lanets',
+  },
+  { '<leader>t', group = '[t]rouble' },
+  {
+    '<leader>tt',
+    '<CMD>TroubleToggle<CR>',
+    desc = '[t]rouble',
+  },
+  {
+    '<leader>tn',
+    '<CMD>lua require("trouble").next({ skip_groups = true, jump = true })<CR>',
+    desc = '[n]ext',
+  },
+  {
+    '<leader>tp',
+    '<CMD>lua require("trouble").previous({ skip_groups = true, jump = true })<CR>',
+    desc = '[p]revious',
+  },
+  { '<leader>g', group = '[g]it' },
+  {
+    '<leader>gc',
+    '<CMD>Telescope git_commits<CR>',
+    desc = 'Checkout [c]ommit',
+  },
+  {
+    '<leader>gC',
+    '<CMD>Telescope git_bcommits<CR>',
+    desc = '[C]heckout commit(for current file)',
+  },
+  {
+    '<leader>gs',
+    '<CMD>Gitsigns stage_hunk<CR>',
+    desc = '[s]tage hunk',
+  },
+  {
+    '<leader>gu',
+    '<CMD>Gitsigns undo_stage_hunk<CR>',
+    desc = '[u]ndo stage hunk',
+  },
+  {
+    '<leader>gr',
+    '<CMD>Gitsigns reset_hunk<CR>',
+    desc = '[r]eset hunk',
+  },
+  {
+    '<leader>gp',
+    '<CMD>Gitsigns preview_hunk<CR>',
+    desc = '[p]review hunk',
+  },
+  {
+    '<leader>gn',
+    '<CMD>Gitsigns next_hunk<CR>',
+    desc = '[n]ext hunk',
+  },
+  {
+    '<leader>gb',
+    '<CMD>Gitsigns prev_hunk<CR>',
+    desc = '[back] hunk',
+  },
+  { '<leader>l', group = 'LSP' },
+  {
+    '<leader>lr',
+    '<CMD>lua vim.lsp.buf.rename()<CR>',
+    desc = '[r]ename',
+  },
+  {
+    '<leader>li',
+    '<CMD>lua vim.lsp.buf.hover()<CR>',
+    desc = '[i]nformation',
+  },
+  {
+    '<leader>ld',
+    '<CMD>lua vim.lsp.buf.definition()<CR>',
+    desc = '[d]efinition',
+  },
+  {
+    '<leader>lD',
+    '<CMD>lua vim.lsp.buf.declaration()<CR>',
+    desc = '[D]eclaration',
+  },
+  {
+    '<leader>lm',
+    '<CMD>lua vim.lsp.buf.implementation()<CR>',
+    desc = 'I[m]plementation',
+  },
+  {
+    '<leader>lq',
+    '<CMD>lua vim.lsp.buf.references()<CR>',
+    desc = '[q]uickfix',
+  },
+  {
+    '<leader>le',
+    '<CMD>lua vim.diagnostic.open_float()<CR>',
+    desc = '[e]rrors',
+  },
+  {
+    '<leader>la',
+    '<CMD>lua vim.lsp.buf.code_action()<CR>',
+    desc = 'Code [a]ction',
+  },
+  { '<leader>L', group = 'Treesitter [L]SP' },
+  {
+    '<leader>Li',
+    function()
+      local type = require('nvim-treesitter.ts_utils').get_node_at_cursor():type()
+      type = type:gsub('^%l', string.upper)
+      type = type:gsub('_', ' ')
+      vim.lsp.util.open_floating_preview({ type })
+    end,
+    desc = '[i]nformation',
+  },
+  { '<leader>m', group = 'Markdown' },
+  {
+    '<leader>mz',
+    '<CMD>ZoteroCite<CR>',
+    desc = '[z]otero cite',
+  },
+  {
+    '<leader>ml',
+    '<CMD>MarkdownFollowLink<CR>',
+    desc = 'Follow Markdown Link',
+  },
+  {
+    '<leader>mt',
+    '<CMD>ThesaurusReplace<CR>',
+    desc = '[t]hesaurus replace',
+  },
+  { '<leader>r', group = '[r]estore session' },
+  {
+    '<leader>rs',
+    '<CMD>lua require("persistence").load()<CR>',
+    desc = '[s]ession for current directory',
+  },
+  {
+    '<leader>rl',
+    '<CMD>lua require("persistence").load({ last = true })<CR>',
+    desc = '[l]ast session',
+  },
+  { '<leader>d', group = '[d]ebug' },
+  {
+    '<leader>dc',
+    '<CMD>lua require("dap").continue()<CR>',
+    desc = '[c]ontinue',
+  },
+  {
+    '<leader>di',
+    '<CMD>lua require("dap").step_into()<CR>',
+    desc = 'Step [i]nto',
+  },
+  {
+    '<leader>do',
+    '<CMD>lua require("dap").step_over()<CR>',
+    desc = 'Step [o]ver',
+  },
+  {
+    '<leader>dO',
+    '<CMD>lua require("dap").step_out()<CR>',
+    desc = 'Step [o]ut',
+  },
+  {
+    '<leader>db',
+    '<CMD>lua require("dap").toggle_breakpoint()<CR>',
+    desc = 'Toggle [b]reakpoint',
+  },
+  {
+    '<leader>dB',
+    '<CMD>lua require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>',
+    desc = 'Set breakpoint',
+  },
+  {
+    '<leader>dt',
+    '<CMD>lua require("dapui").toggle()<CR>',
+    desc = 'DapUI [t]oggle',
+  },
+  {
+    '<leader>y',
+    '<CMD>call setreg("+", @")<CR>',
+    desc = 'Copy to clipboard',
+  },
+})
