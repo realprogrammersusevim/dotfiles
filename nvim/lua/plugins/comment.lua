9ef9a07e5f7ec4fe633b467fb70bc78a4c89ecc8
Jonathan <git@jonathansm.dev>
2024-11-07 18:29:28 -0600

    feat(nvim): add twilight and more lazy loading

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,7 +1,7 @@
 return {
   {
     'echasnovski/mini.comment',
-    event = 'VeryLazy',
+    keys = { 'gc' },
     config = true,
   },
 }
--- nvim/lua/plugins/nabla.lua
+++ nvim/lua/plugins/nabla.lua
@@ -1,8 +1,4 @@
 return {
   'jbyuki/nabla.nvim',
-  ft = 'markdown',
   lazy = true,
-  config = function()
-    require('nabla').enable_virt()
-  end,
 }
