d9caca2b10d3648434d785572a862c2bc8ba4480
Jonathan <git@jonathansm.dev>
2025-01-14 11:31:47 -0600

    style(sketchybar): change clock to 24 hour time

--- sketchybar/items/clock.lua
+++ sketchybar/items/clock.lua
@@ -1,9 +1,6 @@
-local colors = require('colors')
-local settings = require('settings')
-
 local datetime = function()
   -- Get the current date and time formatted as ISO %a, %h %e - %l:%M %p
-  return os.date('%a, %b %d - %I:%M %p')
+  return os.date('%a %b %d %H:%M')
 end
 
 local clock = Sbar.add('item', 'clock', {
