8621f9a51cd3620dfa27dc417ce97ab797ca3ee5
Jonathan <git@jonathansm.dev>
2025-06-30 14:51:05 -0500

    refactor(nvim): switch from none_ls to conform.nvim for formatting

---
+++ nvim/lua/plugins/format.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_format = 'fallback' }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = {
+        lsp_format = 'prefer',
+        timeout_ms = 500,
+      },
+      formatters = {
+        prettier = {
+          prepend_args = { '--prose-wrap', 'always' }
+        }
+      },
+      formatters_by_ft = {
+        markdown = { 'prettier' },
+        yaml = { 'prettier' },
+        bash = { 'shfmt' },
+      }
+    }
+  }
+}
