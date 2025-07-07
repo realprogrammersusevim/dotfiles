ada1786a67d7a63e9fc0326ee4a96fee1c3c4bec
Jonathan <git@jonathansm.dev>
2025-07-07 16:13:16 -0500

    fix(sketchybar): add padding_right to front_app

--- sketchybar/items/front_app.lua
+++ sketchybar/items/front_app.lua
@@ -2,6 +2,9 @@ local front_app = Sbar.add('item', 'front_app', {
   display = 'active',
   icon = { drawing = false },
   updates = true,
+  label = {
+    padding_right = 15,
+  }
 })
 
 front_app:subscribe('front_app_switched', function(env)
