2511e5643230b121e124e540ece282445c14387e
Jonathan <git@jonathansm.dev>
2025-07-30 15:18:38 -0500

    refactor(macos): keep default bluetooth audio values
    I decided since I don't know what the sound edits are really changing,
    and I don't care enough to find out, better to just stick with the
    defaults.

--- macos/setup.sh
+++ macos/setup.sh
@@ -35,24 +35,6 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 # Disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
-# Sound
-
-defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Max (editable)" 80
-defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" 80
-defaults write com.apple.BluetoothAudioAgent "Apple Initial Bitpool (editable)" 80
-defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool" 80
-defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool Max" 80
-defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool Min" 80
-defaults write com.apple.BluetoothAudioAgent "Stream - Flush Ring on Packet Drop (editable)" 0
-defaults write com.apple.BluetoothAudioAgent "Stream - Max Outstanding Packets (editable)" 16
-defaults write com.apple.BluetoothAudioAgent "Stream Resume Delay" "0.75"
-
-defaults write bluetoothaudiod "Enable AAC codec" -bool true
-defaults write bluetoothaudiod "AAC Bitrate" -int 320
-defaults write bluetoothaudiod "AAC max packet size" -int 644
-
-sudo pkill bluetoothd
-
 # Finder
 
 # Finder: show all filename extensions
