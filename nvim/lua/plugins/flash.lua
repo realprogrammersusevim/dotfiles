b3693a02ea65be8732f8a46534da30ddab1ff44c
Jonathan <git@jonathansm.dev>
2026-06-21 16:10:58 -0500

    feat(nvim): add flash.nvim plugin

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      {
+        '<leader>j',
+        mode = { 'n', 'x', 'o' },
+        function() require('flash').jump() end,
+        desc = 'Flash [j]ump'
+      },
+      {
+        '<leader>J',
+        mode = { 'n', 'x', 'o' },
+        function() require('flash').treesitter() end,
+        desc = 'Flash treesitter [J]ump'
+      },
+      {
+        'r',
+        mode = 'o',
+        function() require('flash').remote() end,
+        desc = 'Remote Flash'
+      },
+      {
+        'R',
+        mode = 'o',
+        function()
+          require('flash')
+              .treesitter_search()
+        end,
+        desc = 'Treesitter Search'
+      },
+      {
+        '<c-s>',
+        mode = { 'c' },
+        function() require('flash').toggle() end,
+        desc = 'Toggle Flash Search'
+      },
+    },
+  },
+}
