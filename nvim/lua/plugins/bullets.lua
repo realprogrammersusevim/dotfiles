e278286808350462653e150725351df89130a779
Jonathan <git@jonathansm.dev>
2026-08-25 16:50:43 -0500

    feat: use lua version of bullets

--- nvim/lua/plugins/bullets.lua
+++ nvim/lua/plugins/bullets.lua
@@ -1,10 +1,9 @@
 return {
   {
-    'dkarter/bullets.vim',
+    'bullets-vim/bullets.nvim',
     ft = { 'markdown', 'text', 'typst', 'gitcommit' },
-    config = function()
-      vim.g.bullets_delete_last_bullet_if_empty = 1
-      vim.g.bullets_pad_right = 0
-    end,
+    opts = {
+      enabled_file_types = { 'markdown', 'text', 'typst', 'gitcommit' },
+    },
   },
 }
